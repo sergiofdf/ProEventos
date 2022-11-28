8521333f7598c5cae1de963f01b989e389a0990a
Sérgio Fleury Dias Filho <sergiofdf@gmail.com>
2022-11-28 17:26:21 -0300

    aula 106

--- Front/ProEventos-App/src/app/components/eventos/evento-lista/evento-lista.component.ts
+++ Front/ProEventos-App/src/app/components/eventos/evento-lista/evento-lista.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, TemplateRef } from '@angular/core';
+import { Router } from '@angular/router';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { ToastrService } from 'ngx-toastr';
@@ -40,7 +41,8 @@ public modalRef?: BsModalRef;
     private eventoService: EventoService,
     private modalService: BsModalService,
     private toastr: ToastrService,
-    private spinner: NgxSpinnerService
+    private spinner: NgxSpinnerService,
+    private router: Router
     ) {}
 
   public ngOnInit(): void {
@@ -78,6 +80,10 @@ public modalRef?: BsModalRef;
 
   decline(): void {
     this.modalRef?.hide();
+  }
+
+  detalheEvento(id: number): void {
+    this.router.navigate([`eventos/detalhe/${id}`]);
   }
 
 }
