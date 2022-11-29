8ae6a24816ccf1e6e2cc6bb9ff70a64b3b86afbd
Sérgio Fleury Dias Filho <sergiofdf@gmail.com>
2022-11-29 11:29:58 -0300

    aula 121

--- Front/ProEventos-App/src/app/components/user/registration/registration.component.ts
+++ Front/ProEventos-App/src/app/components/user/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  form!: FormGroup;
+
+  constructor( private fb: FormBuilder ) { }
+
+
+  get f(): any {
+    return this.form.controls;
+  }
 
   ngOnInit(): void {
+    this.validation();
+  }
+
+  private validation(): void {
+    this.form = this.fb.group({
+      primeiroNome: ['', Validators.required],
+      ultimoNome: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      userName: ['', Validators.required],
+      senha: ['', [Validators.required, Validators.minLength(6)]],
+      confirmeSenha: ['', Validators.required],
+    });
   }
 
 }
